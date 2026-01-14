bfd3c81796dd56cd6ce88ec73de0432282b72dd3
lmanuelli <laurent.manuelli@sitec.corsica>
2026-01-14 14:13:12 +0100

    first commit

---
+++ Assets/Script/Pivot/Door.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Script.Pivot
+{
+    public class Door : MonoBehaviour
+    {
+        public bool isOpen;
+        
+        public Transform pivot;
+        public Transform openedReference;
+        public Transform closedReference;
+        [SerializeField] private float openSpeed = 2f;
+
+        private void Update()
+        {
+            var targetRotation = isOpen ? openedReference.localRotation : closedReference.localRotation;
+            pivot.localRotation = Quaternion.Slerp(
+                pivot.localRotation,
+                targetRotation,
+                openSpeed * Time.deltaTime
+            );
+        }
+        
+        
+        public void OpenDoor()
+        {
+            isOpen = true;
+        }
+        public void CloseDoor()
+        {
+            isOpen = false;
+        }
+    }
+}
---
+++ Assets/Script/Pivot/Wall.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Script.Pivot
+{
+    public class Wall : MonoBehaviour
+    {
+        public bool isOpen;
+        
+        public Transform pivot;
+        public Transform openedReference;
+        public Transform closedReference;
+        [SerializeField] private float openSpeed = 2f;
+        
+        
+        private void Update()
+        {
+            var targetRotation = isOpen ? openedReference.localRotation : closedReference.localRotation;
+            pivot.localRotation = Quaternion.Slerp(
+                pivot.localRotation,
+                targetRotation,
+                openSpeed * Time.deltaTime
+            );
+        }
+
+        public void OpenWall()
+        {
+            isOpen = true;
+        }
+        public void CloseWall()
+        {
+            isOpen = false;
+        }
+    }
+}
